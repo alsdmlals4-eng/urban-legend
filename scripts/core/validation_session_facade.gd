544bf580dd9727b542bc27d494b89a124b6062d0
alsdmlals4-eng <alsdmlals4@gmail.com>
2026-08-02 17:29:14 +0900

    refactor: remove unused Session facade

--- scripts/core/validation_session_facade.gd
+++
@@ -1,8 +0,0 @@
-extends "res://scripts/core/validation_session.gd"
-
-const PersistenceSummaryScript := preload("res://scripts/core/validation_persistence_summary.gd")
-
-
-func inspect_persistence() -> Dictionary:
-	var inspected: Dictionary = _repository.inspect()
-	return PersistenceSummaryScript.build(inspected)
