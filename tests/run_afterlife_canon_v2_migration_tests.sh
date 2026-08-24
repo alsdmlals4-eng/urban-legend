acb0b3f999555163c84ffd22082dd1fe1c496bae
alsdmlals4-eng <alsdmlals4@gmail.com>
2026-08-24 12:54:56 +0900

    test: add M01 first-session orchestration to focused suite

--- tests/run_afterlife_canon_v2_migration_tests.sh
+++ tests/run_afterlife_canon_v2_migration_tests.sh
@@ -20,6 +20,7 @@ entrypoints=(
   "res://tests/afterlife_migration/afterlife_real_fixture_contract_test.gd"
   "res://tests/monthly_state/monthly_state_policy_test.gd"
   "res://tests/monthly_state/monthly_state_save_compatibility_test.gd"
+  "res://tests/first_session/m01_first_session_orchestration_test.gd"
 )
 
 run_entrypoint() {
@@ -60,5 +61,5 @@ for entrypoint in "${entrypoints[@]}"; do
   run_entrypoint "$entrypoint"
 done
 
-echo "Afterlife canon v2 migration: 11/11 entrypoints passed"
+echo "Afterlife canon v2 migration: 12/12 entrypoints passed"
 echo "Logs: $LOG_ROOT"
